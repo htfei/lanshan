0d217228f4a6f21a4ebc1d2f0821a4fac68a433d
why2fly <why2fly@aliyun.com>
2019-11-11 23:34:15 +0800

    合并

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# lanshan
-【阑珊影视搜索】众里寻他千百度，蓦然回首，那人却在灯火阑珊处。
+# 阑珊lanshan
+
+一个垂直影视资源搜索引擎，目前实现66影院的电影搜索。
+取自“众里寻他千百度，蓦然回首，那人却在灯火阑珊处”，百度找不到的影视资源，来这里找！
+
+## 关于 阑珊lanshan
+
+阑珊lanshan 是个人最近学习 vue.js 而开发的一个练手项目，没有包装，通过 hls.min.js 库实现hls视频流的在线播放（目前仅实现搜索，可通过点击搜索结果直接跳转播放）。
+
+## 食用方法
+
+1.下载后直接点击“test.html”在浏览器中打开即可。[点击查看gif](./screenshot/show.png)
+
+2.have fun
+
+注: 若加载不出来，则可能是Chrome等浏览器默认禁止跨域,需要开启,设置见[这里](https://www.cnblogs.com/shihaiming/p/10984394.html)
+或者下载chrome插件【access-control-allow-origin】一键解决；
+
+## TODO
+- [ ] 实现更多的网站
+- [ ] 开发chrome插件or油猴脚本的小工具来实现网站测试和配置提交
+- [ ] 实现网站提交反馈
+- [ ] 通过 hls.min.js 库实现hls视频流的在线播放
---
+++ test.js
@@ -0,0 +1,89 @@
+﻿
+var hostlist = [{
+    "baseUrl": "http://66yy.in",
+    "chapterFind": ".lazy&&data-original;.tab-jq&&Text;body&&.videourl@li;a&&Text;a&&href;body&&.h1@Text!无需安装任何插件!倒序↓!顺序↑",
+    "forFearch": false,
+    "id": 1208,
+    "movieFind": "",
+    "pinYinTitle": "66yingyuan",
+    "searchFind": "body&&li;a&&title;a&&href;.other&&Text",
+    "searchUrl": "http://66yy.in/index.php?m=vod-search&wd=**",
+    "title": "66影院",
+    "weight": 676
+  },
+  {
+    "baseUrl": "http://www.o8ys.com",
+    "chapterFind": ".fed-deta-images&&a&&data-original;.fed-part-both&&Text;.fed-matp-v,1&&.fed-play-item@li,2:;a&&Text;a&&href;.fed-matp-v&&li@Text",
+    "forFearch": false,
+    "id": 1200,
+    "movieFind": "",
+    "pinYinTitle": "oubayingshi",
+    "searchFind": ".fed-part-layout&&dl;h1&&Text;a&&href;span,1&&Text",
+    "searchUrl": "http://www.o8ys.com/vodsearch/-------------.html?wd=**",
+    "title": "欧八影视",
+    "weight": 668
+  },
+  {
+    "id": 2,
+    "name": "玖玖资源站",
+    "uri": "https://www.sesejiu.com",
+    "classapi": "http://99zxcj.com/inc/jsonsapi.php?ac=list",
+    "listapi": "http://99zxcj.com/inc/jsonsapi.php?ac=videolist",
+    "type": "综合性资源"
+  }
+]
+
+var vm = new Vue({
+  el: '#app-4',
+  data: {
+    "hostlist": hostlist,
+    "getlist_url": "http://66yy.in/index.php?m=vod-search&wd=**", //&pg=&t=
+    "getlist_rsp": {
+		"page":1,
+		"pagecount":1,
+		"counts":1,
+		"data":[{		
+			"title":"",
+			"url":"",
+			"msg":"",
+			"imageUrl":""
+			}]
+	},
+    "classid": "",
+    "querywd": null,
+	"placeholder": "西行纪",
+    "currenthostindex": 0
+  },
+
+  methods: {
+    querypage: function () {
+      var url = this.getlist_url.replace("**", this.querywd||this.placeholder);
+      console.log(url);
+      this.getlist(url);
+    },
+    getlist: function (url) {
+      this.getlist_rsp.page=1;
+      this.getlist_rsp.pagecount=1;
+      (async () => {
+        try {
+          let response = await fetch(url);
+          var html = await response.text();
+          //const $ = cheerio.load(html);
+		  var doc = new DOMParser().parseFromString(html, "text/html");
+          var list = [].map.call($(doc).find('body li.p1.m1'), function (item) {
+            return {
+              title: $(item).find('a').attr('title'),
+              url: hostlist[0].baseUrl+$(item).find('a').attr('href'),
+              msg: $(item).find('.other').text().trim(),
+              imageUrl: hostlist[0].baseUrl+$(item).find('img.lazy').attr('src')
+            };
+          });
+          console.log(list);
+          this.getlist_rsp.data = list;
+        } catch (e) {
+          console.log("Oops, error", e);
+        }
+      })();
+    }
+  }
+});
